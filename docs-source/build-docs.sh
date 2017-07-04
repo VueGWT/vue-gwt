bbf1efd59c3ac8cd2f149c3027aefbddccd8fb89
adrienbaron <adrien.baron@axellience.com>
2017-07-04 12:38:51 +0200

    Add a script to build documentation

---
+++ docs-source/build-docs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "-> Build examples"
+cd examples
+mvn clean package
+cd ..
+
+echo "-> Copy built examples to Gitbook"
+rm book/dependencies/scripts/*
+cp examples/target/vue-gwt-examples-*/VueGwtExamples/* book/dependencies/scripts/
+
+echo "-> Build Gitbook"
+cd book
+gitbook build
+cd ..
+
+echo "-> Copy built Gitbook to docs"
+cd ..
+rm -rf docs/*
+cp -r docs-source/book/_book/* docs/
+rm docs/dependencies.md # Useless file getting copied
+
+echo "Success!"
