cef323f68afd0f30b013dae24b801c27d0c80491
adrienbaron <adrien.baron@axellience.com>
2017-07-17 11:54:23 +0200

    Add VNodes to VueDirective and it's documentation

--- docs-source/examples/src/main/java/com/axellience/vuegwtexamples/client/examples/focus/FocusDirective.java
+++ docs-source/examples/src/main/java/com/axellience/vuegwtexamples/client/examples/focus/FocusDirective.java
@@ -1,6 +1,7 @@
 package com.axellience.vuegwtexamples.client.examples.focus;
 
 import com.axellience.vuegwt.client.directive.VueDirective;
+import com.axellience.vuegwt.client.vnode.VNode;
 import com.axellience.vuegwt.client.vnode.VNodeDirective;
 import com.axellience.vuegwt.jsr69.directive.annotations.Directive;
 import com.google.gwt.dom.client.Element;
@@ -12,7 +13,7 @@ import com.google.gwt.dom.client.Element;
 public class FocusDirective extends VueDirective
 {
     @Override
-    public void inserted(Element el, VNodeDirective binding)
+    public void inserted(Element el, VNodeDirective binding, VNode vnode)
     {
         el.focus();
     }
--- src/main/java/com/axellience/vuegwt/client/directive/VueDirective.java
+++ src/main/java/com/axellience/vuegwt/client/directive/VueDirective.java
@@ -1,5 +1,6 @@
 package com.axellience.vuegwt.client.directive;
 
+import com.axellience.vuegwt.client.vnode.VNode;
 import com.axellience.vuegwt.client.vnode.VNodeDirective;
 import com.google.gwt.dom.client.Element;
 import jsinterop.annotations.JsType;
@@ -10,19 +11,28 @@ import jsinterop.annotations.JsType;
 @JsType
 public abstract class VueDirective
 {
-    public void bind(Element el, VNodeDirective binding) {
+    public void bind(Element el, VNodeDirective binding, VNode vnode)
+    {
         // Do nothing, override to add behavior
     }
-    public void inserted(Element el, VNodeDirective binding) {
+
+    public void inserted(Element el, VNodeDirective binding, VNode vnode)
+    {
         // Do nothing, override to add behavior
     }
-    public void update(Element el, VNodeDirective binding) {
+
+    public void update(Element el, VNodeDirective binding, VNode vnode, VNode oldVnode)
+    {
         // Do nothing, override to add behavior
     }
-    public void componentUpdated(Element el, VNodeDirective binding) {
+
+    public void componentUpdated(Element el, VNodeDirective binding, VNode vnode, VNode oldVnode)
+    {
         // Do nothing, override to add behavior
     }
-    public void unbind(Element el, VNodeDirective binding) {
+
+    public void unbind(Element el, VNodeDirective binding, VNode vnode)
+    {
         // Do nothing, override to add behavior
     }
 }
