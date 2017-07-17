5d968ad74eaddd686d87e3c896dc0a8db44c652a
adrienbaron <adrien.baron@axellience.com>
2017-07-17 18:05:56 +0200

    Add JavaDoc to VNodeBuilder

--- src/main/java/com/axellience/vuegwt/client/vnode/builder/VNodeBuilder.java
+++ src/main/java/com/axellience/vuegwt/client/vnode/builder/VNodeBuilder.java
@@ -17,32 +17,58 @@ public class VNodeBuilder
         this.function = function;
     }
 
+    /**
+     * Create an empty VNode
+     * @return a new empty VNode
+     */
     public VNode el()
     {
         return this.function.create(null, null, null);
     }
 
+    /**
+     * Create a VNode with the given HTML tag
+     * @param tag HTML tag for the new VNode
+     * @param children Children
+     * @return a new VNode of this tag
+     */
     public VNode el(String tag, Object... children)
     {
         return this.function.create(tag, children, null);
     }
 
+    /**
+     * Create a VNode with the given HTML tag
+     * @param tag HTML tag for the new VNode
+     * @param data Information for the new VNode (attributes...)
+     * @param children Children
+     * @return a new VNode of this tag
+     */
     public VNode el(String tag, VNodeData data, Object... children)
     {
         return this.function.create(tag, data, children);
     }
 
+    /**
+     * Create a VNode with the given Component
+     * @param vueConstructor Constructor for the Component we want
+     * @param children Children
+     * @return a new VNode of this Component
+     */
     public VNode el(VueConstructor<Vue> vueConstructor, Object... children)
     {
-        return this.function.create(vueConstructor,
-            children,
-            null);
+        return this.function.create(vueConstructor, children, null);
     }
 
+    /**
+     * Create a VNode with the given Component
+     * @param vueConstructor Constructor for the Component we want
+     * @param data Information for the new VNode (attributes...)
+     * @param children Children
+     * @return a new VNode of this Component
+     */
     public VNode el(Class<Vue> vueConstructor, VNodeData data, Object... children)
     {
-        return this.function.create(vueConstructor,
-            data,
-            children);
+        return this.function.create(vueConstructor, data, children);
     }
 }
