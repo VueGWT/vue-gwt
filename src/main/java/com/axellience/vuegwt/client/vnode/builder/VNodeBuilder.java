d24fc8b4193f35910259a33a7b5c4e813c0536c6
adrienbaron <adrien.baron@axellience.com>
2017-08-07 13:22:16 +0200

    Update VNodeBuilder to support VueFactories

--- src/main/java/com/axellience/vuegwt/client/vnode/builder/VNodeBuilder.java
+++ src/main/java/com/axellience/vuegwt/client/vnode/builder/VNodeBuilder.java
@@ -1,8 +1,10 @@
 package com.axellience.vuegwt.client.vnode.builder;
 
+import com.axellience.vuegwt.client.VueGWT;
 import com.axellience.vuegwt.client.component.VueComponent;
 import com.axellience.vuegwt.client.vnode.VNode;
 import com.axellience.vuegwt.client.vnode.VNodeData;
+import com.axellience.vuegwt.client.vue.VueFactory;
 import com.axellience.vuegwt.client.vue.VueJsConstructor;
 
 /**
@@ -50,18 +52,7 @@ public class VNodeBuilder
     }
 
     /**
-     * Create a VNode with the given Component
-     * @param vueJsConstructor Constructor for the Component we want
-     * @param children Children
-     * @return a new VNode of this Component
-     */
-    public VNode el(VueJsConstructor<VueComponent> vueJsConstructor, Object... children)
-    {
-        return this.function.create(vueJsConstructor, children, null);
-    }
-
-    /**
-     * Create a VNode with the given Component
+     * Create a VNode with the given {@link VueComponent}
      * @param vueComponentClass Class for the {@link VueComponent} we want
      * @param data Information for the new VNode (attributes...)
      * @param children Children
@@ -69,6 +60,28 @@ public class VNodeBuilder
      */
     public VNode el(Class<VueComponent> vueComponentClass, VNodeData data, Object... children)
     {
-        return this.function.create(vueComponentClass, data, children);
+        return el(VueGWT.getJsConstructor(vueComponentClass), data, children);
+    }
+
+    /**
+     * Create a VNode with the {@link VueComponent} of the given {@link VueFactory}
+     * @param vueFactory {@link VueFactory} for the Component we want
+     * @param children Children
+     * @return a new VNode of this Component
+     */
+    public VNode el(VueFactory<VueComponent> vueFactory, Object... children)
+    {
+        return el(vueFactory.getJsConstructor(), children, null);
+    }
+
+    /**
+     * Create a VNode with the {@link VueComponent} of the given {@link VueJsConstructor}
+     * @param vueJsConstructor {@link VueJsConstructor} for the Component we want
+     * @param children Children
+     * @return a new VNode of this Component
+     */
+    public VNode el(VueJsConstructor<VueComponent> vueJsConstructor, Object... children)
+    {
+        return this.function.create(vueJsConstructor, children, null);
     }
 }
