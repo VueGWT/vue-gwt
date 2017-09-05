ae1e9b22bf41d434c081c8ac9333d47d8f852936
adrienbaron <adrien.baron@axellience.com>
2017-09-05 17:10:51 +0200

    Make CustomizeOptions an interface instead of an abstract class

--- src/main/java/com/axellience/vuegwt/client/component/options/CustomizeOptions.java
+++ src/main/java/com/axellience/vuegwt/client/component/options/CustomizeOptions.java
@@ -3,7 +3,7 @@ package com.axellience.vuegwt.client.component.options;
 /**
  * @author Adrien Baron
  */
-public abstract class CustomizeOptions
+public interface CustomizeOptions
 {
-    public abstract void customizeOptions(VueComponentOptions vueComponentOptions);
+    void customizeOptions(VueComponentOptions vueComponentOptions);
 }
