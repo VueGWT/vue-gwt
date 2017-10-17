ea7f342a303f6b3a050d5acdc221bf6e5a4a31da
adrienbaron <adrien.baron@axellience.com>
2017-10-17 15:13:30 +0200

    Add a render method to ErrorMakerComponent example to remove Vue warning

--- docs-source/examples/src/main/java/com/axellience/vuegwtexamples/client/examples/errorboundary/ErrorMakerComponent.java
+++ docs-source/examples/src/main/java/com/axellience/vuegwtexamples/client/examples/errorboundary/ErrorMakerComponent.java
@@ -1,17 +1,26 @@
 package com.axellience.vuegwtexamples.client.examples.errorboundary;
 
+import com.axellience.vuegwt.client.component.HasRender;
 import com.axellience.vuegwt.client.component.VueComponent;
 import com.axellience.vuegwt.client.component.hooks.HasCreated;
+import com.axellience.vuegwt.client.vnode.VNode;
+import com.axellience.vuegwt.client.vnode.builder.VNodeBuilder;
 import com.axellience.vuegwt.jsr69.component.annotations.Component;
 import com.axellience.vuegwtexamples.client.examples.common.Todo;
 
 @Component(hasTemplate = false)
-public class ErrorMakerComponent extends VueComponent implements HasCreated
+public class ErrorMakerComponent extends VueComponent implements HasCreated, HasRender
 {
     @Override
     public void created()
     {
         Todo nullTodo = null;
         nullTodo.getText(); // This will break
+    }
+
+    @Override
+    public VNode render(VNodeBuilder builder)
+    {
+        return builder.el("div");
     }
 }
