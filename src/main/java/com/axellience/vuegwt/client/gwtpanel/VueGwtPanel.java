418c2ec29eb40d06f19f40d7f7641776fec74e10
Justin <justin.trentesaux@axellience.com>
2017-10-19 17:59:43 +0200

    Fix VueGwtPanel to be able to remove it from another widget
    Change the inheritance of SimplePanel to widget because it
    must not contain another widget. Also use a temporary div
    element to mount the component on it to fix the issue.
    
    Fixes #7

--- src/main/java/com/axellience/vuegwt/client/gwtpanel/VueGwtPanel.java
+++ src/main/java/com/axellience/vuegwt/client/gwtpanel/VueGwtPanel.java
@@ -4,15 +4,16 @@ import com.axellience.vuegwt.client.VueGWT;
 import com.axellience.vuegwt.client.component.VueComponent;
 import com.axellience.vuegwt.client.vue.VueFactory;
 import com.axellience.vuegwt.client.vue.VueJsConstructor;
-import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * Wrap a {@link VueComponent} instance in a GWT Panel
- * @author Adrien Baron
  */
-public class VueGwtPanel<T extends VueComponent> extends SimplePanel
+public class VueGwtPanel<T extends VueComponent> extends Widget
 {
-    private T vueComponentInstance;
+    private final T vueComponentInstance;
 
     public VueGwtPanel(VueFactory<T> vueFactory)
     {
@@ -22,6 +23,7 @@ public class VueGwtPanel<T extends VueComponent> extends SimplePanel
     public VueGwtPanel(VueJsConstructor<T> vueJsConstructor)
     {
         super();
+        setElement(Document.get().createDivElement());
         vueComponentInstance = vueJsConstructor.instantiate();
     }
 
@@ -29,16 +31,30 @@ public class VueGwtPanel<T extends VueComponent> extends SimplePanel
     {
         this(VueGWT.getFactory(vueClass));
     }
-
-    @Override
-    protected void onAttach()
-    {
-        super.onAttach();
-        vueComponentInstance.$mount(getElement());
-    }
-
+    
     public T vue()
     {
-        return this.vueComponentInstance;
+        return vueComponentInstance;
+    }
+
+    @Override
+    protected void onLoad()
+    {
+        super.onLoad();
+        if (!isMounted()) {
+            mountVueComponent();
+        }
+    }
+
+    private boolean isMounted()
+    {
+        return getElement().hasChildNodes();
+    }
+
+    private void mountVueComponent()
+    {
+        Element vueElement = Document.get().createDivElement();
+        getElement().appendChild(vueElement);
+        vueComponentInstance.$mount(vueElement);
     }
 }
