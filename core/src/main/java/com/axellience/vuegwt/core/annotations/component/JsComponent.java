ebcecb9987e48efec85b9b326d85920e100964a6
adrienbaron <adrien.baron@axellience.com>
2017-11-18 10:48:44 +0100

    Update @JsComponent to support extending Options object

--- core/src/main/java/com/axellience/vuegwt/core/annotations/component/JsComponent.java
+++ core/src/main/java/com/axellience/vuegwt/core/annotations/component/JsComponent.java
@@ -14,5 +14,5 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
 @Retention(SOURCE)
 public @interface JsComponent
 {
-
+    String value();
 }
--- processors/src/main/java/com/axellience/vuegwt/processors/component/factory/VueJsComponentFactoryGenerator.java
+++ processors/src/main/java/com/axellience/vuegwt/processors/component/factory/VueJsComponentFactoryGenerator.java
@@ -1,18 +1,17 @@
 package com.axellience.vuegwt.processors.component.factory;
 
 import com.axellience.vuegwt.core.annotations.component.JsComponent;
+import com.axellience.vuegwt.core.client.Vue;
 import com.axellience.vuegwt.core.client.component.VueComponent;
 import com.axellience.vuegwt.core.client.tools.VueGWTTools;
 import com.axellience.vuegwt.core.client.vue.VueFactory;
-import com.axellience.vuegwt.core.client.vue.VueJsConstructor;
-import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeSpec.Builder;
 import elemental2.dom.DomGlobal;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
+import jsinterop.base.Js;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Modifier;
@@ -21,10 +20,9 @@ import javax.tools.Diagnostic.Kind;
 import java.util.LinkedList;
 import java.util.List;
 
-import static com.axellience.vuegwt.core.generation.GenerationNameUtil.getPackage;
-
 /**
- * Generate {@link VueFactory} from the user {@link VueComponent} classes annotated by {@link
+ * Generate {@link VueFactory} from the user {@link VueComponent} classes
+ * annotated by {@link
  * JsComponent}.
  * @author Adrien Baron
  */
@@ -43,33 +41,59 @@ public class VueJsComponentFactoryGenerator extends AbstractVueComponentFactoryG
         {
             messager.printMessage(Kind.ERROR,
                 component.asType().toString()
-                    + " Js Component must have @JsType annotation with isNative to true.");
+                    + " @JsComponent must have a @JsType annotation with isNative to true");
+            return null;
         }
+        if (!JsPackage.GLOBAL.equals(jsType.namespace()))
+        {
+            messager.printMessage(Kind.ERROR,
+                component.asType().toString()
+                    + " @JsType annotation on @JsComponent must have namespace set to JsPackage.GLOBAL");
+            return null;
+        }
+
+        JsComponent jsComponent = component.getAnnotation(JsComponent.class);
 
         MethodSpec.Builder initBuilder =
             MethodSpec.methodBuilder("init").addModifiers(Modifier.PRIVATE);
 
-        String namespace = jsType.namespace();
-        if (JsPackage.GLOBAL.equals(namespace))
-            namespace = "";
-        else if ("<auto>".equals(namespace))
-            namespace = getPackage(component) + ".";
+        if ("Function".equals(jsType.name()))
+        {
+            initForComponentConstructor(jsComponent, initBuilder);
+        }
+        else if ("Object".equals(jsType.name()))
+        {
+            initForComponentOptions(jsComponent, initBuilder);
+        }
         else
-            namespace += ".";
-
-        String name = jsType.name();
-        if ("<auto>".equals(name))
-            name = component.getSimpleName().toString();
-
-        // Static init block
-        initBuilder.addStatement("jsConstructor = ($T) $T.getDeepValue($T.window, $S)",
-            ParameterizedTypeName.get(ClassName.get(VueJsConstructor.class),
-                ClassName.get(component.asType())),
-            VueGWTTools.class,
-            DomGlobal.class,
-            namespace + name);
+        {
+            messager.printMessage(Kind.ERROR,
+                component.asType().toString()
+                    + " @JsType annotation on @JsComponent must have name set to either Function or Object.");
+        }
 
         vueFactoryBuilder.addMethod(initBuilder.build());
         return new LinkedList<>();
+    }
+
+    private void initForComponentOptions(JsComponent jsComponent, MethodSpec.Builder initBuilder)
+    {
+        initBuilder.addStatement(
+            "jsConstructor = $T.extend($T.cast($T.getDeepValue($T.window, $S)))",
+            Vue.class,
+            Js.class,
+            VueGWTTools.class,
+            DomGlobal.class,
+            jsComponent.value());
+    }
+
+    private void initForComponentConstructor(JsComponent jsComponent,
+        MethodSpec.Builder initBuilder)
+    {
+        initBuilder.addStatement("jsConstructor = $T.cast($T.getDeepValue($T.window, $S))",
+            Js.class,
+            VueGWTTools.class,
+            DomGlobal.class,
+            jsComponent.value());
     }
 }
