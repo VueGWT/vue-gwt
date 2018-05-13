e1a0819e97605da358a5dd3c32440d1426067522
Justin <justin.trentesaux@axellience.com>
2018-05-13 16:47:30 +0200

    Add tests for v-show

---
+++ tests/src/test/javascript/components/conditionalrendering/vshow.spec.js
@@ -0,0 +1,32 @@
+import {expect} from 'chai'
+import {
+  createAndMountComponent, destroyComponent, getElement, onGwtReady,
+  onNextTick
+} from '../../vue-gwt-tests-utils'
+
+describe('v-show', () => {
+  let component;
+
+  beforeEach(() => onGwtReady().then(() => {
+    component = createAndMountComponent(
+        'com.axellience.vuegwt.tests.client.components.conditionalrendering.vshow.VShowTestComponent'
+    );
+  }));
+
+  afterEach(() => {
+    destroyComponent(component);
+  });
+
+  it('should hide its element if its condition is false', () => {
+    const showElement = getElement(component, '#show-element');
+    expect(showElement.style.display).to.equals('none');
+  });
+
+  it('should show its element if its condition is true', () => {
+    const showElement = getElement(component, '#show-element');
+    component.showCondition = true;
+    return onNextTick(() => {
+      expect(showElement.style.display).to.be.empty;
+    });
+  });
+});
