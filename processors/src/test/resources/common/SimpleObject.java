20f331c7e1ffcf1cfcaaa2e9a71a1e686ee26ff0
adrienbaron <adrien@abaron.net>
2018-06-09 11:56:00 +0200

    fix: template expressions with line breaks on windows
    Template expressions ({{ }}) with line breaks were not parsed correctly
    on windows.
    
    Fix #38

--- processors/src/test/resources/common/SimpleObject.java
+++ processors/src/test/resources/common/SimpleObject.java
@@ -2,4 +2,8 @@ package common;
 
 public class SimpleObject {
   String text;
+
+  public String getText() {
+    return text;
+  }
 }
---
+++ processors/src/test/resources/templateparser/compileresult/MustacheExpressionComponentExposedType.java
@@ -0,0 +1,18 @@
+package templateparser;
+
+@Generated(
+    value = "com.axellience.vuegwt.processors.component.ComponentExposedTypeGenerator",
+    comments = "https://github.com/Axellience/vue-gwt"
+)
+@JsType(
+    namespace = "VueGWTExposedTypesRepository",
+    name = "templateparser_MustacheExpressionComponent"
+)
+public class MustacheExpressionComponentExposedType extends MustacheExpressionComponent {
+  @JsMethod
+  @SuppressWarnings("unusable-by-js")
+  public String exp$0() {
+    // MustacheExpressionComponent.html, line 1
+    return VueGWTTools.templateExpressionToString(simpleObject.getText());
+  }
+}
