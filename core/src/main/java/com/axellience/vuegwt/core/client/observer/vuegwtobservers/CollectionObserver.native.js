96f15ac53bd99feab3060f95a80d32431e0cac59
adrienbaron <adrien.baron@axellience.com>
2018-07-15 09:37:39 +0200

    feat: make collection observation work in GWT3

---
+++ core/src/main/java/com/axellience/vuegwt/core/client/observer/vuegwtobservers/CollectionObserver.native.js
@@ -0,0 +1,19 @@
+const ListJavaType = goog.require('java.util.List');
+goog.exportSymbol("java.util.List.prototype.clear", ListJavaType.prototype.clear);
+goog.exportSymbol("java.util.List.prototype.remove", ListJavaType.prototype.remove);
+goog.exportSymbol("java.util.List.prototype.removeAtIndex", ListJavaType.prototype.removeAtIndex);
+goog.exportSymbol("java.util.List.prototype.removeAll", ListJavaType.prototype.removeAll);
+goog.exportSymbol("java.util.List.prototype.retainAll", ListJavaType.prototype.retainAll);
+goog.exportSymbol("java.util.List.prototype.add", ListJavaType.prototype.add);
+goog.exportSymbol("java.util.List.prototype.addAll", ListJavaType.prototype.addAll);
+goog.exportSymbol("java.util.List.prototype.addAtIndex", ListJavaType.prototype.addAtIndex);
+goog.exportSymbol("java.util.List.prototype.addAllAtIndex", ListJavaType.prototype.addAllAtIndex);
+goog.exportSymbol("java.util.List.prototype.setAtIndex", ListJavaType.prototype.setAtIndex);
+
+const SetJavaType = goog.require('java.util.Set');
+goog.exportSymbol("java.util.Set.prototype.clear", SetJavaType.prototype.clear);
+goog.exportSymbol("java.util.Set.prototype.remove", SetJavaType.prototype.remove);
+goog.exportSymbol("java.util.Set.prototype.removeAll", SetJavaType.prototype.removeAll);
+goog.exportSymbol("java.util.Set.prototype.retainAll", SetJavaType.prototype.retainAll);
+goog.exportSymbol("java.util.Set.prototype.add", SetJavaType.prototype.add);
+goog.exportSymbol("java.util.Set.prototype.addAll", SetJavaType.prototype.addAll);
---
+++ core/src/main/java/com/axellience/vuegwt/core/client/observer/vuegwtobservers/MapObserver.native.js
@@ -0,0 +1,7 @@
+const MapJavaType = goog.require('java.util.Map');
+goog.exportSymbol("java.util.Map.prototype.clear", MapJavaType.prototype.clear);
+goog.exportSymbol("java.util.Map.prototype.remove", MapJavaType.prototype.remove);
+goog.exportSymbol("java.util.Map.prototype.put", MapJavaType.prototype.put);
+goog.exportSymbol("java.util.Map.prototype.putIfAbsent", MapJavaType.prototype.putIfAbsent);
+goog.exportSymbol("java.util.Map.prototype.putAll", MapJavaType.prototype.putAll);
+goog.exportSymbol("java.util.Map.prototype.replace", MapJavaType.prototype.replace);
