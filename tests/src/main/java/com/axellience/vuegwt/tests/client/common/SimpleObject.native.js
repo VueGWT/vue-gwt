eb94c812f4d26ac08b9da8be0b9c5457e5f43119
adrienbaron <adrien.baron@axellience.com>
2018-07-17 10:16:04 +0200

    tests: add a native.js for SimpleObject for GWT3

---
+++ tests/src/main/java/com/axellience/vuegwt/tests/client/common/SimpleObject.native.js
@@ -0,0 +1,2 @@
+goog.exportProperty(SimpleObject.prototype, "getStringProperty", SimpleObject.prototype.getStringProperty);
+goog.exportProperty(SimpleObject.prototype, "setStringProperty", SimpleObject.prototype.setStringProperty);
