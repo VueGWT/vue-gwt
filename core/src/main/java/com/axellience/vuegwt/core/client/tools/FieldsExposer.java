11dba38d5ea65671bfea90facb68527b30225215
adrienbaron <adrien.baron@axellience.com>
2018-07-18 23:29:16 +0200

    fix: expose FieldsExposer as FieldsExposer and not Object
    This make it necessary to declare it as extern in Closure, but removes
    warning from Closure about methods not existing on Object.

--- core/src/main/java/com/axellience/vuegwt/core/client/tools/FieldsExposer.java
+++ core/src/main/java/com/axellience/vuegwt/core/client/tools/FieldsExposer.java
@@ -3,7 +3,7 @@ package com.axellience.vuegwt.core.client.tools;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
-@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+@JsType(isNative = true, namespace = JsPackage.GLOBAL)
 public class FieldsExposer {
   public static native <T> T v();
   public static native void e(Object... values);
