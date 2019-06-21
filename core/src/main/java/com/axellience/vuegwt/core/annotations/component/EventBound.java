b49922b6bc3fb0ca2ff5bdc346daf50eead77206
Candide Kemmler <hi@candide.me>
2019-06-21 12:19:10 +0200

    fixes nashorn NPE

---
+++ core/src/main/java/com/axellience/vuegwt/core/annotations/component/EventBound.java
@@ -0,0 +1,12 @@
+package com.axellience.vuegwt.core.annotations.component;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface EventBound {
+}
