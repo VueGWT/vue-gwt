14f2a1afee7faee351f3d031a9eb1b2d0cd76f16
adrienbaron <adrien@abaron.net>
2020-01-15 18:32:58 +0000

    feature: add Vue externs

---
+++ core/src/main/java/com/axellience/vuegwt/core/client/Vue.js
@@ -0,0 +1,243 @@
+/**
+ * @externs
+ */
+var Vue = {
+  "cid": {},
+  "_uid": "",
+  $options: {},
+  "compile": function () {
+  },
+  "component": function () {
+  },
+  "delete": function () {
+  },
+  "directive": function () {
+  },
+  "extend": function () {
+  },
+  "filter": function () {
+  },
+  "mixin": function () {
+  },
+  "nextTick": function () {
+  },
+  "options": {
+    "_base": {
+      "cid": {},
+      "compile": function () {
+      },
+      "component": function () {
+      },
+      "delete": function () {
+      },
+      "directive": function () {
+      },
+      "extend": function () {
+      },
+      "filter": function () {
+      },
+      "mixin": function () {
+      },
+      "nextTick": function () {
+      },
+      "options": {},
+      "set": function () {
+      },
+      "use": function () {
+      },
+      "util": {
+        "defineReactive": function () {
+        },
+        "extend": function () {
+        },
+        "mergeOptions": function () {
+        },
+        "warn": function () {
+        }
+      },
+      "version": {}
+    },
+    "components": {
+      "KeepAlive": {
+        "abstract": {},
+        "created": function () {
+        },
+        "destroyed": function () {
+        },
+        "mounted": function () {
+        },
+        "name": {},
+        "props": {
+          "exclude": {},
+          "include": {},
+          "max": {}
+        },
+        "render": function () {
+        }
+      },
+      "Transition": {
+        "abstract": {},
+        "name": {},
+        "props": {
+          "appear": function () {
+          },
+          "appearActiveClass": function () {
+          },
+          "appearClass": function () {
+          },
+          "appearToClass": function () {
+          },
+          "css": function () {
+          },
+          "duration": {},
+          "enterActiveClass": function () {
+          },
+          "enterClass": function () {
+          },
+          "enterToClass": function () {
+          },
+          "leaveActiveClass": function () {
+          },
+          "leaveClass": function () {
+          },
+          "leaveToClass": function () {
+          },
+          "mode": function () {
+          },
+          "name": function () {
+          },
+          "type": function () {
+          }
+        },
+        "render": function () {
+        }
+      },
+      "TransitionGroup": {
+        "beforeUpdate": function () {
+        },
+        "methods": {
+          "hasMove": function () {
+          }
+        },
+        "props": {
+          "appear": function () {
+          },
+          "appearActiveClass": function () {
+          },
+          "appearClass": function () {
+          },
+          "appearToClass": function () {
+          },
+          "css": function () {
+          },
+          "duration": {},
+          "enterActiveClass": function () {
+          },
+          "enterClass": function () {
+          },
+          "enterToClass": function () {
+          },
+          "leaveActiveClass": function () {
+          },
+          "leaveClass": function () {
+          },
+          "leaveToClass": function () {
+          },
+          "moveClass": function () {
+          },
+          "name": function () {
+          },
+          "tag": function () {
+          },
+          "type": function () {
+          }
+        },
+        "render": function () {
+        },
+        "updated": function () {
+        }
+      }
+    },
+    "directives": {
+      "model": {
+        "componentUpdated": function () {
+        },
+        "inserted": function () {
+        }
+      },
+      "show": {
+        "bind": function () {
+        },
+        "unbind": function () {
+        },
+        "update": function () {
+        }
+      }
+    },
+    "filters": {}
+  },
+  "set": function () {
+  },
+  "use": function () {
+  },
+  "util": {
+    "defineReactive": function () {
+    },
+    "extend": function () {
+    },
+    "mergeOptions": function () {
+    },
+    "warn": function () {
+    }
+  },
+  "version": {}
+};
+Vue.prototype = {
+  "$delete": function () {
+  },
+  "$destroy": function () {
+  },
+  "$emit": function () {
+  },
+  "$forceUpdate": function () {
+  },
+  "$mount": function () {
+  },
+  "$nextTick": function () {
+  },
+  "$off": function () {
+  },
+  "$on": function () {
+  },
+  "$once": function () {
+  },
+  "$set": function () {
+  },
+  "$watch": function () {
+  },
+  "__patch__": function () {
+  },
+  "_init": function () {
+  },
+  "_render": function () {
+  },
+  "_update": function () {
+  }
+};
+
+/**
+ * @externs
+ */
+var VueObserver = {
+  "observeArray": function () {
+  },
+  "dep": {
+    "notify": function () {
+    }
+  }
+};
+
+/**
+ * @externs
+ */
+var F = {v: function () {}, e: function () {}};
