c30675be580d170898195afeeaafba75c412cbcc
adrienbaron <adrien@abaron.net>
2020-03-21 13:42:17 +0000

    tests: use Vue GWT embeded Vue runtime for J2CL tests

--- tests/tests-runner/src/test/javascript/karma.conf.ci.j2cl.js
+++ tests/tests-runner/src/test/javascript/karma.conf.ci.j2cl.js
@@ -7,7 +7,6 @@ module.exports = function (config) {
   config.set({
     files: [
       'src/test/javascript/components/**/*.js',
-      'src/test/javascript/lib/vue.runtime.js',
       {
         pattern: "../tests-app-wrapper-j2cl/target/vue-gwt-tests-app-wrapper-j2cl-*/**/*.j2cl.js",
         watched: false
