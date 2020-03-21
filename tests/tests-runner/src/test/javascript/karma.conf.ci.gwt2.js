0d49d2ea4697b49a677bd3a6687341a62133fcc0
adrienbaron <adrien@abaron.net>
2020-03-21 13:37:01 +0000

    tests: improve karma config for J2CL and GWT2

--- tests/tests-runner/src/test/javascript/karma.conf.ci.gwt2.js
+++ tests/tests-runner/src/test/javascript/karma.conf.ci.gwt2.js
@@ -4,14 +4,19 @@ module.exports = function (config) {
   // Load base config
   baseConfig(config);
 
+  // Override base config
   config.set({
     files: [
       'src/test/javascript/components/**/*.js',
-      'src/test/javascript/lib/vue.runtime.js',
       {
-        pattern: "../tests-app-wrapper-j2cl/target/vue-gwt-tests-app-wrapper-j2cl-*/**/*.j2cl.js",
+        pattern: "../tests-app-wrapper-gwt2/target/vue-gwt-tests-app-wrapper-gwt2-*/**/*.nocache.js",
         watched: false
       },
+      {
+        pattern: "../tests-app-wrapper-gwt2/target/vue-gwt-tests-app-wrapper-gwt2-*/**/*.cache.js",
+        watched: false,
+        included: false
+      }
     ],
     singleRun: true,
     autoWatch: false
