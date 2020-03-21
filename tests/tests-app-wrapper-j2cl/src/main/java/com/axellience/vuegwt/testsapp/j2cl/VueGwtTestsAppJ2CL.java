9b13d1ac558eaffa271122d60ddc4dfe3014ac4e
adrienbaron <adrien@abaron.net>
2020-03-21 14:47:02 +0000

    tests: rename testappgwt2 to testapp.gwt2 and testappj2cl to testapp.j2cl

--- tests/tests-app-wrapper-j2cl/src/main/java/com/axellience/vuegwt/testsapp/j2cl/VueGwtTestsAppJ2CL.java
+++ tests/tests-app-wrapper-j2cl/src/main/java/com/axellience/vuegwt/testsapp/j2cl/VueGwtTestsAppJ2CL.java
@@ -1,4 +1,4 @@
-package com.axellience.vuegwt.testsappj2cl;
+package com.axellience.vuegwt.testsapp.j2cl;
 
 import com.axellience.vuegwt.core.client.VueGWT;
 import com.axellience.vuegwt.testsapp.client.VueGwtTestApp;
@@ -7,6 +7,6 @@ public class VueGwtTestsAppJ2CL {
 
   public void onModuleLoad() {
     VueGWT.init();
-    VueGwtTestApp.bootstrap();
+    VueGwtTestsApp.bootstrap();
   }
 }
