db2be45844be8a714b39535d59b1b30cc46e82e0
adrienbaron <adrien@abaron.net>
2022-01-02 17:17:22 +0000

    add test for interface default method access from template

--- tests/tests-app/src/main/java/com/axellience/vuegwt/testsapp/client/components/inheritance/ChildComponent.java
+++ tests/tests-app/src/main/java/com/axellience/vuegwt/testsapp/client/components/inheritance/ChildComponent.java
@@ -3,10 +3,11 @@ package com.axellience.vuegwt.testsapp.client.components.inheritance;
 import com.axellience.vuegwt.core.annotations.component.Component;
 import com.axellience.vuegwt.core.annotations.component.Computed;
 import com.axellience.vuegwt.core.annotations.component.Data;
+import com.axellience.vuegwt.testsapp.client.common.InterfaceWithDefaultMethod;
 import jsinterop.annotations.JsMethod;
 
 @Component
-public class ChildComponent extends ParentComponent {
+public class ChildComponent extends ParentComponent implements InterfaceWithDefaultMethod {
   @Data
   protected String childData = "CHILD_INITIAL_DATA";
 
